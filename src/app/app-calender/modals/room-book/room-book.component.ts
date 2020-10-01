913e7f737f3c7718bbaf0cb5a1381ee8399f5df9
EkhlasMridha <ekhlasmridha@gmail.com>
2020-10-01 18:31:11 +0600

    booking form error check solveed

--- src/app/app-calender/modals/room-book/room-book.component.ts
+++ src/app/app-calender/modals/room-book/room-book.component.ts
@@ -1,4 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  OnInit,
+} from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -6,12 +12,14 @@ import {
   Validators,
 } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { filter } from 'rxjs/operators';
 import { FormService } from 'src/app/shared-services/utilities/form.service';
 
 @Component({
   selector: 'app-room-book',
   templateUrl: './room-book.component.html',
   styleUrls: ['./room-book.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RoomBookComponent implements OnInit {
   data: any;
@@ -29,7 +37,8 @@ export class RoomBookComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) data: any,
     private formBuilder: FormBuilder,
-    private formService: FormService
+    private formService: FormService,
+    private ch: ChangeDetectorRef
   ) {
     this.data = data;
   }
@@ -60,21 +69,37 @@ export class RoomBookComponent implements OnInit {
   }
 
   createBookingForm() {
-    return this.formBuilder.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      dateRange: this.formBuilder.group({
-        startDate: ['', Validators.required],
-        endDate: ['', Validators.required],
-      }),
-      adults: ['', Validators.required],
-      children: [''],
-      chf: ['', Validators.required],
-    });
+    return this.formBuilder.group(
+      {
+        firstname: ['', Validators.required],
+        lastname: ['', Validators.required],
+        dateRange: this.formBuilder.group({
+          startDate: ['', Validators.required],
+          endDate: ['', Validators.required],
+        }),
+        adults: ['', Validators.required],
+        children: [''],
+        chf: ['', Validators.required],
+      },
+      {
+        validators: [this.watchDateRangeErrorState('startDate', 'endDate')],
+      }
+    );
   }
 
   onSubmit() {
     console.log(this.bookingForm.value);
     this.formService.checkFormStatus(this.bookingForm);
+  }
+
+  watchDateRangeErrorState(value1: string, value2: string) {
+    return (formGroup: FormGroup) => {
+      if (
+        formGroup.controls.dateRange.value[value1] == '' ||
+        formGroup.controls.dateRange.value[value1] == null
+      ) {
+        return formGroup.controls.dateRange.setErrors({ required: true });
+      }
+    };
   }
 }
--- src/app/shared-services/utilities/form.service.ts
+++ src/app/shared-services/utilities/form.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { filter, map } from 'rxjs/operators';
+import * as _ from 'lodash';
 
 @Injectable({
   providedIn: 'root',
@@ -13,17 +14,15 @@ export class FormService {
     errorObservers: object,
     errorTypeGenerator: (type: string, owner: string) => any
   ) {
-    // console.log(formGorup.controls);
     Object.keys(formGorup.controls).forEach((field) => {
-      // console.log(field);
       errorObservers[field] = formGorup.controls[field].statusChanges.pipe(
         filter((status) => status === 'INVALID'),
         map(() => {
+          // console.log(formGorup.controls[field]);
           if (!formGorup.controls[field].errors) {
             return null;
           }
           for (let errName in formGorup.controls[field].errors) {
-            // console.log(errName);
             let errorType = errorTypeGenerator(errName, field.toString());
             return errorType;
           }
@@ -34,7 +33,6 @@ export class FormService {
 
   checkFormStatus(formGroup: FormGroup) {
     Object.keys(formGroup.controls).forEach((field) => {
-      console.log(field);
       formGroup.controls[field].updateValueAndValidity();
     });
   }
