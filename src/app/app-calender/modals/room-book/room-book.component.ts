30fdad5198fe295a75598e1ab0c716ec53bea4fa
EkhlasMridha <ekhlasmridha@gmail.com>
2020-10-01 17:02:07 +0600

    added form control booking

--- src/app/app-calender/modals/room-book/room-book.component.ts
+++ src/app/app-calender/modals/room-book/room-book.component.ts
@@ -1,5 +1,12 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormService } from 'src/app/shared-services/utilities/form.service';
 
 @Component({
   selector: 'app-room-book',
@@ -8,11 +15,66 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class RoomBookComponent implements OnInit {
   data: any;
-  constructor(@Inject(MAT_DIALOG_DATA) data: any) {
+  bookingForm: FormGroup;
+
+  errorObservers$ = {
+    firstname: '',
+    lastname: '',
+    dateRange: '',
+    adults: '',
+    children: '',
+    chf: '',
+  };
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) data: any,
+    private formBuilder: FormBuilder,
+    private formService: FormService
+  ) {
     this.data = data;
   }
 
   ngOnInit(): void {
     console.log(this.data);
+    this.bookingForm = this.createBookingForm();
+    this.formService.handleFormError(
+      this.bookingForm,
+      this.errorObservers$,
+      this.errorTypeGenerator
+    );
+  }
+
+  errorTypeGenerator(type: string, owner: string) {
+    switch (owner) {
+      case 'firstname':
+        return 'First name is required';
+      case 'lastname':
+        return 'Last name is required';
+      case 'dateRange':
+        return 'Date range required';
+      case 'adults':
+        return 'Set number of adults';
+      case 'chf':
+        return 'Set amount';
+    }
+  }
+
+  createBookingForm() {
+    return this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      dateRange: this.formBuilder.group({
+        startDate: ['', Validators.required],
+        endDate: ['', Validators.required],
+      }),
+      adults: ['', Validators.required],
+      children: [''],
+      chf: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.bookingForm.value);
+    this.formService.checkFormStatus(this.bookingForm);
   }
 }
