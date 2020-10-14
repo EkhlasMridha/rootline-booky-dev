099d99388840003d8933ed69ed4d7ee7537be77c
= <=>
2020-10-14 09:29:00 +0600

    code cleaned

--- src/app/app-settings/components/settigns/settigns.component.ts
+++ src/app/app-settings/components/settigns/settigns.component.ts
@@ -15,14 +15,12 @@ export class SettignsComponent implements OnInit {
   ngOnInit(): void {
     this.primaryButton = this.primaryButton.bind(this);
     this.settingService.getProfile().subscribe(res=>{
-      console.log(res);
       this.bexioToken = res.bexioToken;
     })
   }
   saveToken(){
     let ref = this.dialogService.openConfirmationModal({isLoader:true,loaderText:"Saving token ...",disableClose:true});
     this.settingService.changeBexioToken(this.bexioToken).subscribe(res=>{
-      console.log(res);
       ref.close();
       this.dialogService.dispose();
       this.dialogService.openConfirmationModal({headerText:"Bexio token saved succesfully",primaryButtonName:"Okay",primaryEvent:this.primaryButton});
